d858165ad39fe5ce0eaa7501a75f0e793ef9703c
Vladas Zakrevskis <146100@gmail.com>
2021-11-04 23:14:30 +0200

    added tools

---
+++ deps.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+import os
+
+
+def _get_home():
+    if "HOME" in os.environ:
+        return os.environ["HOME"]
+    return os.path.expanduser("~")
+    
+
+home = _get_home()
+deps_path = home + "/.rdeps/"
+
+tools_path = deps_path + "tools/"
+
+this_path = os.path.dirname(os.path.abspath(__file__))
+
+
+def run(string):
+    print(string)
+    if os.system(string):
+        raise Exception("Shell script has failed")
+
+
+def clone(rep, destination=""):
+    if not os.path.exists(destination):
+        run("git clone --recursive https://github.com/vladasz/" + rep + " " + destination)
+
+
+clone("tools", tools_path)
+
+
+def link_deps():
+    try:
+        print("Symlimk: " + deps_path + " to: " + this_path + "/.rdeps")
+        os.symlink(deps_path, this_path + "/.rdeps")
+    except FileExistsError:
+        print("exists")
+
+
+link_deps()
