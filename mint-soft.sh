84b00dc5e874cab0ee3277034c30da5ade339243
Vladas Zakrevskis <146100@gmail.com>
2022-01-24 20:49:58 +0200

    mint soft

---
+++ mint-soft.sh
@@ -0,0 +1,9 @@
+flatpak install telegram -y
+flatpak install spotify -y
+flatpak install slack -y
+flatpak install clion -y
+flatpak install wps -y
+flatpak install gitkraken -y
+flatpak install com.Valvesoftware.steam -y
+
+#com.visualstudio.code
