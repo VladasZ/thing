b60036ab6d55b616890ffb09cf14170251de1d1f
Vladas Zakrevskis <146100@gmail.com>
2022-01-25 14:38:06 +0200

    zoom

--- soft.ps1
+++ soft.ps1
@@ -7,5 +7,6 @@ choco install spotify
 choco install slack
 choco install telegram
 choco install autohotkey
+choco install zoom
 Invoke-WebRequest https://github.com/alacritty/alacritty/releases/download/v0.9.0/Alacritty-v0.9.0-installer.msi -OutFile alacritty.msi
 .\alacritty.msi
