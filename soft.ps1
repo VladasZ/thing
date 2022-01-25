aa3178f1c24fc9f2a6daea429855746aec658ee7
Vladas Zakrevskis <146100@gmail.com>
2022-01-25 18:46:10 +0200

    win transmission

--- soft.ps1
+++ soft.ps1
@@ -11,6 +11,7 @@ choco install zoom
 choco install lightshot
 choco install krita
 choco install steam
+choco install transmission
 
 Invoke-WebRequest https://github.com/alacritty/alacritty/releases/download/v0.9.0/Alacritty-v0.9.0-installer.msi -OutFile alacritty.msi
 .\alacritty.msi
