ad2270a62aa202b2240182b572a314e7bc2f8615
Vladas Zakrevskis <146100@gmail.com>
2022-01-28 16:32:12 +0200

    ditched allacritty

--- soft.ps1
+++ soft.ps1
@@ -13,6 +13,3 @@ choco install krita
 choco install steam
 choco install transmission
 choco install crystaldiskmark
-
-Invoke-WebRequest https://github.com/alacritty/alacritty/releases/download/v0.9.0/Alacritty-v0.9.0-installer.msi -OutFile alacritty.msi
-.\alacritty.msi
