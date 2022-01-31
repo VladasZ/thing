0af65ca459a7a9f4553d13dfb61d33c489ef1b3e
Vladas <146100@gmail.com>
2022-01-31 13:26:26 +0200

    pretty

--- space.sh
+++ space.sh
@@ -1,6 +1,10 @@
-echo macdonald
-ssh macdonald 'df -h'
-echo macarthur
-ssh macarthur 'df -h'
-echo maclaren
-ssh maclaren 'df -h'
+echo
+echo "                             macdonald"
+ssh macdonald 'df -h | head -2'
+echo
+echo "                             macarthur"
+ssh macarthur 'df -h | head -2'
+echo
+echo "                             maclaren"
+ssh maclaren 'df -h | head -2'
+echo
