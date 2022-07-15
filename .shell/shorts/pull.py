e4571342babd001c21d58e8e3afa2cdd65f0eb83
Vladas Zakrevskis <146100@gmail.com>
2022-07-15 07:31:17 +0000

    better push pop

--- .shell/shorts/pull.py
+++ .shell/shorts/pull.py
@@ -2,5 +2,7 @@
 
 import os
 
+os.system("git stash")
 os.system("git pull")
 os.system("git pull --recurse-submodules --jobs=10")
+os.system("git stash pop")
--- .shell/shorts/push.py
+++ .shell/shorts/push.py
@@ -8,7 +8,10 @@ args = ""
 for arg in sys.argv[1:]:
     args += arg + " "
 
+os.system("git stash")
 os.system("git pull")
+os.system("git stash pop")
+
 os.system("git add -A")
 os.system("git commit -m \"" + args + "\"")
 os.system("git push")
