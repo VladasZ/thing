dc94bbf47f96ec28c9dccd47cf06452945a82bc6
Vladas Zakrevskis <146100@gmail.com>
2022-10-04 10:54:41 +0000

    prent

--- .shell/shorts/upload.py
+++ .shell/shorts/upload.py
@@ -4,4 +4,5 @@ import os
 
 print("ELASTIO_ARTIFACTS_SOURCE: " + os.environ["ELASTIO_ARTIFACTS_SOURCE"])
 
+print("AWS_PROFILE=assuriodev cargo trix artifacts upload")
 os.system("AWS_PROFILE=assuriodev cargo trix artifacts upload")
