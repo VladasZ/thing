aaaa20622d6c842d4d8ee696de1e99e6c7ee20b1
Vladas <146100@gmail.com>
2023-03-11 20:47:33 +0200

    also taplo

--- .shell/shorts/fe.py
+++ .shell/shorts/fe.py
@@ -3,3 +3,10 @@
 import subprocess
 
 subprocess.run(["cargo", "fmt", "--all"])
+
+try:
+    subprocess.run(["taplo", "--version"], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+except subprocess.CalledProcessError:
+    subprocess.run(["cargo", "install", "taplo-cli"])
+
+subprocess.run(["taplo", "fmt"])
