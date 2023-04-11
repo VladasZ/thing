28732b62ebe8c8d4af32c18af833424f69f2fb9a
Vladas <146100@gmail.com>
2023-04-11 19:23:13 +0300

    cucu

---
+++ generated/debug/build/libc-490d98fbbc34669f/build_script_build-490d98fbbc34669f.d
@@ -0,0 +1,5 @@
+/Users/vladas/thing/./generated/debug/build/libc-490d98fbbc34669f/build_script_build-490d98fbbc34669f: /Users/vladas/.cargo/registry/src/index.crates.io-6f17d22bba15001f/libc-0.2.134/build.rs
+
+/Users/vladas/thing/./generated/debug/build/libc-490d98fbbc34669f/build_script_build-490d98fbbc34669f.d: /Users/vladas/.cargo/registry/src/index.crates.io-6f17d22bba15001f/libc-0.2.134/build.rs
+
+/Users/vladas/.cargo/registry/src/index.crates.io-6f17d22bba15001f/libc-0.2.134/build.rs:
