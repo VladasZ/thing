bb60eaf5eb9e2ff39725cea95ea8f52fb960544f
Vladas Zakrevskis <146100@gmail.com>
2023-04-13 16:32:19 +0200

    redock stop

--- .shell/shorts/redock.py
+++ .shell/shorts/redock.py
@@ -4,13 +4,23 @@ import sys
 import subprocess
 
 status = False
+stop = False
+
 
 if len(sys.argv) > 1:
     status = sys.argv[1] == "st"
+    stop = sys.argv[1] == "stop"
 
-if not status:
+
+if status:
+    subprocess.run(["sudo", "docker-compose", "ps"])
+    exit(0)
+
+if stop:
     subprocess.run(["sudo", "docker-compose", "stop"])
-    subprocess.run(["sudo", "docker-compose", "rm", "-y"])
-    subprocess.run(["sudo", "docker-compose", "up", "-d"])
+    exit(0)
 
+subprocess.run(["sudo", "docker-compose", "stop"])
+subprocess.run(["sudo", "docker-compose", "rm", "-y"])
+subprocess.run(["sudo", "docker-compose", "up", "-d"])
 subprocess.run(["sudo", "docker-compose", "ps"])
