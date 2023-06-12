54ea677013e7e1ae938b610a28c3f8bc02d2ef50
Vladas <146100@gmail.com>
2023-06-12 10:13:15 +0300

    old cb

--- .shell/shorts/cb.py
+++ .shell/shorts/cb.py
@@ -15,19 +15,19 @@ os.system("cargo fmt --all")
 if len(sys.argv) > 1:
     os.system("cargo build -p" + sys.argv[1])
     exit(0)
-else:
-
-    # Run the `cargo workspaces list` command and get the output
-    output = subprocess.check_output(['cargo', 'workspaces', 'list']).decode('utf-8')
-
-    # Extract the project names from the output
-    project_names = [line.split(' ')[0] for line in output.split('\n') if line]
-
-    print(project_names)
-
-    # Run `cargo build -p` for each project
-    for project_name in project_names:
-        subprocess.run(['cargo', 'build', '-p', project_name])
+# else:
+#
+#     # Run the `cargo workspaces list` command and get the output
+#     output = subprocess.check_output(['cargo', 'workspaces', 'list']).decode('utf-8')
+#
+#     # Extract the project names from the output
+#     project_names = [line.split(' ')[0] for line in output.split('\n') if line]
+#
+#     print(project_names)
+#
+#     # Run `cargo build -p` for each project
+#     for project_name in project_names:
+#         subprocess.run(['cargo', 'build', '-p', project_name])
 
 
 os.system("cargo build")
