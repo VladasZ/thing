9631e1a1d400211db57030585ea53bde8e8fa183
Vladas <146100@gmail.com>
2023-08-01 10:51:47 +0300

    proxy

--- .shell/shorts/fwd.py
+++ .shell/shorts/fwd.py
@@ -12,6 +12,6 @@ if pg:
     exit(0)
 
 if k:
-    os.system("ssh -L 8080:127.0.0.1:8080 -L 30100:127.0.0.1:30100 pi")
-    # k proxy -p 8080
+    os.system("ssh -L 8001:127.0.0.1:8001 -L 30100:127.0.0.1:30100 pi")
+    # k proxy
     exit(0)
