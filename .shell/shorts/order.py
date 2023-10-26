8e8be69e9e7db6283c012c54c83f7b0955287b2a
Vladas Zakrevskis <146100@gmail.com>
2023-10-27 00:44:45 +0300

    test in release

--- .shell/shorts/order.py
+++ .shell/shorts/order.py
@@ -7,6 +7,7 @@ os.system("cargo install typos-cli")
 
 os.system("cargo +nightly fmt --all")
 os.system("cargo test --all")
+os.system("cargo test --all --release")
 os.system("cargo build --all")
 os.system("cargo clippy --all -- -A clippy::mismatched_target_os -A clippy::module_inception")
 os.system("typos")
