32d149523e5832cc3db9123ea7a025e10eb9548e
Vladas Zakrevskis <146100@gmail.com>
2024-03-12 15:48:16 +0200

    win lazygit

--- soft.ps1
+++ soft.ps1
@@ -1,16 +1,24 @@
 #no clion
 #no deezer
-choco install gitkraken
-choco install slack
-choco install telegram
-choco install autohotkey
-choco install zoom
-choco install lightshot
-choco install krita
-choco install steam
-choco install transmission
-choco install crystaldiskmark
-choco install speccy
-choco install scanner
-choco install alacritty
-choco install linkshellextension
+# choco install gitkraken
+# choco install slack
+# choco install telegram
+# choco install autohotkey
+# choco install zoom
+# choco install lightshot
+# choco install krita
+# choco install steam
+# choco install transmission
+# choco install crystaldiskmark
+# choco install speccy
+# choco install scanner
+# choco install alacritty
+# choco install linkshellextension
+
+# Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
+# Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression
+
+# refreshenv
+
+scoop bucket add extras
+scoop install lazygit
