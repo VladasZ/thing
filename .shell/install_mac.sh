8086502f2ed50351fb39fa02d3d30b34313b6e21
Vladas <146100@gmail.com>
2024-03-12 18:08:36 +0200

    added shell settings

---
+++ .shell/install_mac.sh
@@ -0,0 +1,13 @@
+
+brew update && brew upgrade
+
+brew install htop
+brew install ncdu
+brew install gh
+brew install lazygit
+brew install hashicorp/tap/terraform
+brew install awscli 
+brew install helix
+brew install taplo
+
+cargo install zellij
---
+++ .shell/shell.sh
@@ -0,0 +1,20 @@
+
+echo Helloy
+
+PROMPT='v %1~ %# '
+
+export PATH=$PATH:~/dev/thing/.shell/shorts
+
+alias z=zellij
+alias dotf='terraform apply -auto-approve'
+alias untf='terraform destroy -auto-approve'
+
+alias te='cd ~/dev/test_engine/'
+
+alias d='sudo docker'
+
+alias tf=terraform
+alias l=lazygit
+
+alias al='hx ~/.ssh/shell.sh'
+
