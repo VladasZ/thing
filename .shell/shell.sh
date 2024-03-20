f576a85ec6cd49f0b4b716a50b748b563496aea7
Vladas Zakrevskis <146100@gmail.com>
2024-03-20 04:20:53 +0200

    hy th

--- .shell/shell.sh
+++ .shell/shell.sh
@@ -10,6 +10,7 @@ alias dotf='terraform apply -auto-approve'
 alias untf='terraform destroy -auto-approve'
 
 alias te='cd ~/dev/test_engine/'
+alias th='cd ~/dev/thing'
 alias hx=helix
 
 alias d='sudo docker'
@@ -18,6 +19,7 @@ alias tf=terraform
 alias l=lazygit
 
 alias al='hx ~/dev/thing/.shell/shell.sh'
+alias hy='hx ~/dev/thing/.shell/hyprland.conf'
 
 alias install_mac=~/dev/thing/.shell/install_mac.sh
 alias install_lin=~/dev/thing/.shell/install_lin.sh
