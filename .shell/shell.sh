41dc0c260b2353133f07f4aca2a442907b7821a1
Vladas Zakrevskis <146100@gmail.com>
2024-05-24 16:19:02 +0300

    auto alacritty ln

--- .shell/shell.sh
+++ .shell/shell.sh
@@ -5,6 +5,8 @@ PROMPT='v %1~ %# '
 
 export PATH=$PATH:~/dev/thing/.shell/shorts
 
+ln -sf ~/dev/thing/.shell/.alacritty.toml ~/.alacritty.toml
+
 alias z=zellij
 alias dotf='terraform apply -auto-approve'
 alias untf='terraform destroy -auto-approve'
