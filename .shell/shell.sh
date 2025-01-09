8517e1c47ffe2b1a43b8caa8a780ead98a0176cb
Vladas Zakrevskis <146100@gmail.com>
2025-01-09 18:38:20 +0200

    removed some stuff

--- .shell/shell.sh
+++ .shell/shell.sh
@@ -20,7 +20,6 @@ alias untf='terraform destroy -auto-approve'
 
 alias te='cd ~/dev/test-engine/'
 alias th='cd ~/dev/thing'
-alias hx=helix
 
 alias d='sudo docker'
 
@@ -29,25 +28,6 @@ alias l=lazygit
 
 alias al='hx ~/dev/thing/.shell/shell.sh'
 alias hy='hx ~/dev/thing/.shell/hyprland.conf'
-
-alias install_mac=~/dev/thing/.shell/install_mac.sh
-alias install_lin=~/dev/thing/.shell/install_lin.sh
-
-target_link=~/.config/zed
-
-if [ ! -e "$target_link" ]; then
-    if [[ "$(uname)" == "Darwin" ]]; then
-        source_path=~/dev/thing/.shell/zed
-    elif [[ "$(uname)" == "Linux" ]]; then
-        source_path=~/dev/thing/.shell/zed_lin
-    else
-        echo "Unsupported operating system"
-        exit 1
-    fi
-
-    ln -sf $source_path $target_link
-    echo "Zed link: OK"
-fi
 
 function clone {
     cargo run --manifest-path ~/dev/thing/Cargo.toml -p clone --release --target-dir ~/dev/thing/target -- "$@"
