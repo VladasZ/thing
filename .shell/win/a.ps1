9c51d145b2acc6b04c27e66c8dd602f70e76fa23
Vladas Zakrevskis <146100@gmail.com>
2025-05-19 14:30:17 +0300

    pack on win

--- .shell/win/a.ps1
+++ .shell/win/a.ps1
@@ -81,6 +81,22 @@ function order {
   py $HOME/dev/thing/.shell/shorts/order.py
 }
 
+function unpack {
+    py $HOME/dev/thing/.shell/shorts/unpack $args
+}
+
+function pack {
+    py $HOME/dev/thing/.shell/shorts/pack $args
+}
+
+function pack5 {
+    py $HOME/dev/thing/.shell/shorts/pack5 $args
+}
+
+function unpack_in_folder {
+    py $HOME/dev/thing/.shell/shorts/unpack_in_folder
+}
+
 function k {
     kubectl $args
 }
