fe5e34f8285e6f23c350f3b5e4131136a608dfbc
Vladas Zakrevskis <146100@gmail.com>
2025-12-15 22:47:48 +0200

    added some casks

--- shell/macos_setup.sh
+++ shell/macos_setup.sh
@@ -1,7 +1,32 @@
 brew install nushell
 brew install starship
 brew install lazygit
+brew install btop
+brew install helm
 brew install --cask obsidian
+brew install --cask wezterm
+brew install --cask jetbrains-toolbox
+brew install --cask visual-studio-code
+brew install --cask transmission
+brew install --cask discord
+brew install --cask zoom
+brew install --cask docker-desktop
+brew install --cask lens
+brew install --cask steam
+brew install --cask vlc
+brew install --cask deezer
+brew install --cask spotify
+brew install --cask telegram
+brew install --cask bambu-studio
+brew install --cask alt-tab
+brew install --cask appcleaner
+brew install --cask balenaetcher
+brew install --cask localsend
+brew install --cask pearcleaner
+brew install --cask hiddenbar
+brew install --cask monitorcontrol
+brew install --cask gitkraken
+brew install --cask battery
 
 # open nu then
 
