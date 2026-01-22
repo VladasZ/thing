545d77e9b5731d1b23ec35cb75ea4667f4c84080
Vladas Zakrevskis <146100@gmail.com>
2026-01-22 18:05:01 +0200

    more settings

--- shell/arch/3-after-setup.sh
+++ shell/arch/3-after-setup.sh
@@ -3,7 +3,7 @@
 set -euxo pipefail
 
 sudo pacman -Suy nushell git base-devel rustup lazygit ncdu alsa-lib starship micro nano helix duf yazi btop unzip gping speedtest-cli protobuf --noconfirm
-sudo pacman -Suy docker-buildx --noconfirm
+sudo pacman -Suy docker-buildx unzip --noconfirm
 
 sudo sed -i '/\[multilib\]/,/Include/ s/^#//' /etc/pacman.conf
 
