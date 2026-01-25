d4f32f629c0cfea1b9a94421d8de95eddda6ed32
Vladas Zakrevskis <146100@gmail.com>
2026-01-25 12:08:44 +0200

    fixed slink on unix

--- shell/shorts/slink.py
+++ shell/shorts/slink.py
@@ -1,26 +1,28 @@
 #!/usr/bin/env python3
 
 import os
-import sys
 import platform
+import sys
+
 
 def create_symlink(src, dst):
     target_is_directory = os.path.isdir(src)
 
-    if platform.system() == 'Windows':
+    if platform.system() == "Windows":
         try:
             os.symlink(src, dst, target_is_directory=target_is_directory)
             print(f"Symlink created: {src} -> {dst}")
         except:
             a = 4
     else:
-        # try:
-        os.symlink(src, dst)
-        print(f"Symlink created: {src} -> {dst}")
-        # except:
-        #     print(f"Failed to create symlink: {src} -> {dst}")
+        try:
+            os.symlink(src, dst)
+            print(f"Symlink created: {src} -> {dst}")
+        except:
+            a = 4
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     src = sys.argv[1]
     dst = sys.argv[2]
 
